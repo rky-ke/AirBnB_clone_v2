5801e1ece00069e6f52f65eb0a58e38629ee61bf
rky-ke <ronaldyegon15@gmail.com>
2024-01-25 16:24:07 +0000

    /hbnb_static/ added

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -18,7 +18,7 @@ echo -e "\e[1;32m directories created\e[0m"
 echo
 
 #--adds test string
-echo "<h1>Welcome to www.iamrkyegon.tech</h1>" > /data/web_static/releases/test/index.html
+echo "<h1>Welcome to www.iamrkyegon.tech</h1>" > sudo /data/web_static/releases/test/index.html
 echo -e "\e[1;32m Test String added\e[0m"
 echo
 
@@ -27,24 +27,35 @@ sudo rm -f /data/web_static/current
 
 # Create a new symbolic link
 sudo ln -s /data/web_static/releases/test/ /data/web_static/current
-sudo chown -hR ubuntu:ubuntu /data
+sudo chown -R ubuntu:ubuntu /data
 
 # Configure Nginx
-cat <<EOF | sudo tee /etc/nginx/sites-available/default > /dev/null
+cat <<EOF | sudo tee /etc/nginx/sites-available/default
 server {
-    listen 80;
+    listen 80 default_server;
+    listen [::]:80 default_server;
     server_name _;
-    add_header X-Served-By \$hostname;
+    add_header X-Served-By 126591-web-01;    
 
     location / {
         root /var/www/html;
         index index.html;
     }
-        location /hbnb_static/ {
+
+    location /redirect_me {
+        return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;
+    }
+    location /hbnb_static {
         alias /data/web_static/current/;
     }
 
+    error_page 404 /404.html;
+    location /404 {
+        root /var/www/html;
+        internal;        
+    }
 }
+
 EOF
 
 #--restart NGINX
