e5c864ca00844275e9464a493253b69e96aa10e7
rky-ke <ronaldyegon15@gmail.com>
2024-02-04 18:04:35 +0300

    HBNB

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -17,6 +17,10 @@ def hello_hbnb():
 	"""Displays 'Hello HBNB!'"""
 	return "Hello HBNB!"
 
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+	return "HBNB"
+
 
 if __name__ == "__main__":
 	app.run(host='0.0.0.0')
