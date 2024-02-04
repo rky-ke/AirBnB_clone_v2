f1c790b91275a46d469b48155a8948d788cdd8f4
rky-ke <ronaldyegon15@gmail.com>
2024-02-04 17:47:35 +0300

    Hello Flask!

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,16 +1,21 @@
 #!/usr/bin/python3
-"""
-This module contains the main Flask application for MyFlaskApp.
-"""
+"""Starts a Flask web application.
 
+The application listens on 0.0.0.0, port 5000.
+Routes:
+    /: Displays 'Hello HBNB!'
+"""
 
 from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/', strict_slashes=False)
-def hello_hbnb():
-    return 'Hello HBNB!'
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+@app.route("/", strict_slashes=False)
+def hello_hbnb():
+	"""Displays 'Hello HBNB!'"""
+	return "Hello HBNB!"
+
+
+if __name__ == "__main__":
+	app.run(host='0.0.0.0')
