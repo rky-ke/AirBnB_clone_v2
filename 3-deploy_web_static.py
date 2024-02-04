27c90eb1f141fd88f42ccdf958d7b34f409f8245
rky-ke <ronaldyegon15@gmail.com>
2024-02-04 13:45:43 +0300

    Fabric files for task 3 and 4

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-# Fabric script (based on the file 2-do_deploy_web_static.py) that creates and distributes an archive to your web servers,
+# Fabric script that creates and distributes to your web servers,
 import os.path
 from datetime import datetime
 from fabric.api import env, local, put, run
