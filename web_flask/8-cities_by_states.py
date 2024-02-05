532af529bfb92f258e0c129c6a9c495b582236c8
rky-ke <ronaldyegon15@gmail.com>
2024-02-05 19:32:36 +0300

    Cities by states

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -14,14 +14,14 @@ from models import storage, State
 app = Flask(__name__)
 
 
-@app.route("/states_list", strict_slashes=False)
-def states_list():
+@app.route("/cities_by_states", strict_slashes=False)
+def cities_by_states():
     """Displays an HTML page with a list of all State objects in DBStorage.
 
     States are sorted by name.
     """
     states = storage.all(State)
-    return render_template('7-states_list.html', states=states)
+    return render_template('8-cities_by_states.html', states=states)
 
 
 @app.teardown_appcontext
