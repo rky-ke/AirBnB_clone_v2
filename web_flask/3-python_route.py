ce1059aef43ac6987b125ca4afea62593315be26
rky-ke <ronaldyegon15@gmail.com>
2024-02-05 14:08:52 +0300

    Python is cool

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -29,6 +29,7 @@ def c_text(text):
     text = escape(text).replace('_', ' ')
     return 'C {}'.format(text)
 
+@app.route("/python", strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
 def python_text(text="is cool"):
     # Replace underscores with spaces in the text variable
