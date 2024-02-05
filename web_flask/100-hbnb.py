f9018692cdb102f00ebf104a40a172efc0c3c03c
rky-ke <ronaldyegon15@gmail.com>
2024-02-05 19:58:36 +0300

    HBNB is alive

--- web_flask/100-hbnb.py
+++ web_flask/100-hbnb.py
@@ -3,22 +3,23 @@
 
 The application listens on 0.0.0.0, port 5000.
 Routes:
-    /hbnb_filters: HBnB HTML filters page.
+    /hbnb: HBnB home page.
 """
-from models import storage, State, Amenity
+from models import storage, State, Amenity, Place
 from flask import Flask
 from flask import render_template
 
 app = Flask(__name__)
 
 
-@app.route("/hbnb_filters", strict_slashes=False)
-def hbnb_filters():
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
     """Displays the main HBnB filters HTML page."""
     states = storage.all(State)
     amenities = storage.all(Amenity)
-    return render_template("10-hbnb_filters.html",
-                           states=states, amenities=amenities)
+    places = storage.all(Place)
+    return render_template("100-hbnb.html",
+                           states=states, amenities=amenities, places=places)
 
 
 @app.teardown_appcontext
